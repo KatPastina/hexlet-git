1ddb34d1f6506e6db076cefe6e34bdefc622fd01
Екатерина Пастина <kate.pastina@gmail.com>
2024-01-23 01:56:28 +0300

    исправлено

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,1 +1,10 @@
 from capitalize import capitalize
+
+
+if capitalize('hello') != 'Hello':
+    raise Exception('Функция работаетневерно!')
+
+if capitalize('') != '':
+    raise Exception('Функция работает неверно!')
+
+print('Все тесты пройдены!')
